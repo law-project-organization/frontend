6b4362a98c36ead697775155641ec89c20223952
이윤호 <134574255+dldbsgh00005@users.noreply.github.com>
2025-07-28 15:37:49 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
       <div>
-          <h1>법률 서비스 김앤장입니다.</h1>
+          <h1>법률 서비스 김앤장입니다. ggggg</h1>
           <br/>
           <h2>Hello, World</h2>
       </div>
